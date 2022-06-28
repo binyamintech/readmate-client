ea70b433bacb0913498c9295cd67de87c9e8c850
Shaul Attie <solyattie13@gmail.com>
2022-06-28 10:03:25 +0300

    10h

--- src/pages/TrainReading/Level/Exercise/index.js
+++ src/pages/TrainReading/Level/Exercise/index.js
@@ -2,24 +2,29 @@ import React from 'react'
 import './style.css';
 import Headphone from './Level_Headphone.png'
 import FooterStart from '../../../../components/common/FooterStart'
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 
 const index = () => {
-const BookName = "Harry Potter" // i need to receive this info fromm shachar
-const title = 'Start Now'
-const textTitle = "With your headphones on,"
-const text = "Continue reading" + BookName + "book at the pace of the sound, for the next 3 minutes – we will notify you when time is up."
-const rout = 'http://localhost:3000/train_reading_level/exercise_clock'
+  const BookName = "Harry Potter" // i need to receive this info fromm shachar
+  const title = 'Start Now'
+  const textTitle = "With your headphones on,"
+  const text = "Continue reading" + BookName + "book at the pace of the sound, for the next 3 minutes – we will notify you when time is up."
+  const rout = '/train_reading_level/exercise_clock'
 
-function onclickHandler(){
-  window.location.href = rout
-}
+  function onclickHandler() {
+    // window.location.href = rout
+    console.log('button')
+  }
   return (
     <div className='level_page'>
       {/* <h1>Level Exercise</h1> */}
       <img src={Headphone} alt="Headphone" />
-      <FooterStart title={title} textTitle={textTitle} text={text} rout={rout}/>
-      <button onClick={onclickHandler}>next</button> // this onclick dont exist, the path will be send throw props.rout to the component
+      <FooterStart title={title} textTitle={textTitle} text={text} rout={rout} />
+        <Link to={rout}>
+          <button onClick={onclickHandler}>next</button> 
+          {/* // this onclick dont exist, the path will be send throw props.rout to the component */}
+        </Link>
     </div>
   )
 }
--- src/pages/TrainReading/Level/ExerciseClock/index.js
+++ src/pages/TrainReading/Level/ExerciseClock/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './style.css';
 import SoundBar from "./SoundBar.png"
+import { Link } from 'react-router-dom';
+
 // import Clock from '../../../../components/common/Clock'
 
 
 const index = () => {
-  const rout = 'http://localhost:3000/train_reading_level/rate'
+  const rout = '/train_reading_level/rate'
 
   function onclickHandler() {
-    window.location.href = rout
+    // window.location.href = rout
+    console.log('button')
   }
-
 
   return (
     <div className="level_page">
@@ -18,7 +20,10 @@ const index = () => {
       {/* <Clock/> */}
       <h1>Clock</h1>
       <h1>12 LPM</h1>
-      <button onClick={onclickHandler}>next</button> // this onclick dont exist, the path will be send throw props.rout to the component
+      <Link to={rout}>
+        <button onClick={onclickHandler}>next</button>
+        {/* // this onclick dont exist, the path will be send throw props.rout to the component */}
+      </Link>
       <img src={SoundBar} />
     </div>
   )
