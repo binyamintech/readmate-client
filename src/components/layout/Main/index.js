d8899332127c551654935fde3f14b6fd2d5ebfab
efrat5454 <efrat5454@gmail.com>
2022-06-28 11:01:24 +0300

    Route

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -1,9 +1,14 @@
 import Test from "../../test/Test";
-import Header from "../header";
 import Nav from "../Nav";
 // import Popup from "../popup/Popup";
 import { Outlet, Route, Routes } from "react-router-dom";
 import TeamA from "../../teams/TeamA";
+import Page1 from "../../../pages/Assessments/Page1";
+import Page6 from "../../../pages/Assessments/Page6";
+import Page5 from "../../../pages/Assessments/page5";
+import Page4 from "../../../pages/Assessments/Page4";
+import Page3 from "../../../pages/Assessments/Page3";
+import Header from "../Header";
 // import TextArea from "../common/TextArea/Index";
 // import Context from "../../../context";
 
@@ -16,6 +21,11 @@ function Main() {
       <Routes>
         {/* Team A */}
         <Route path="teama">
+          <Route path="page1" element={<Page1 />} />
+          <Route path="page3" element={<Page3 />} />
+          <Route path="page4" element={<Page4 />} />
+          <Route path="page5" element={<Page5 />} />
+          <Route path="page6" element={<Page6 />} />
           <Route index element={<TeamA />} />
           <Route path="components-path" element={<>team's components</>} />
         </Route>
