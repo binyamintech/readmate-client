977fcac25cfbe648d0f29465943b5d0af5469d5d
josh goldfrad <joshgoldfrad@gmail.com>
2022-06-28 20:10:52 +0300

    form saves data missing cssbutton and db and rate info

--- src/components/common/Questions/index.js
+++ src/components/common/Questions/index.js
@@ -12,26 +12,27 @@ export default function Questions() {
   const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const body= {
-      questionOne:formData.get("questionOne"),
-      questionTwo:formData.get("questionTwo"),
-      questionThree:formData.get("questionThree"),
-      questionFour:formData.get("questionFour")
-    }
+    const body = {
+      questionOne: formData.get("questionOne"),
+      questionTwo: formData.get("questionTwo"),
+      questionThree: formData.get("questionThree"),
+      questionFour: formData.get("questionFour")
+    };
+    // console.log(e.currentTarget)
+    console.log(body) 
     console.log(e.currentTarget)
-    console.log(body)
-
+    // isFormValid(body)
     navigate("/result");
-    // console.log(formData);
   };
 
 
-  // const isFormValid = () => {
-  //   const { questionOne, questionTwo, questionThree, questionFour } = formData;
-  //   return questionOne && questionTwo && questionThree && questionFour;
-  //   // if (questionOne === "" || !questionTwo || !questionThree || !questionFour) {
-  //   //   console.log(formData);
-  //   // }
+  // const isFormValid = (props) => {
+  //   if (!props) {
+  //       console.log("bonanza");
+  //     }
+  //     // else{
+  //     //   return !isFormValid;
+  //     // }
   // };
 
   // const sendToDb = (e) => {
@@ -50,14 +51,15 @@ export default function Questions() {
         [name]: value,
       };
     });
+    onSubmit()
   };
 
-  const joshClick = (e) => {
-    // onChange(e)
-    onSubmit(e)
-    // if (isFormValid && sendToDb) {
-    // }
-  }
+  // const joshClick = (e) => {
+  //   onChange(e)
+  //   onSubmit(e)
+  //   // if (isFormValid && sendToDb) {
+  //   // }
+  // }
 
   return (
     <div>
@@ -68,40 +70,42 @@ export default function Questions() {
             Consectetur fames feugiat interdum morbi placerat in.
             Leo commodo maecenas donec cursus aenean scelerisque eu.
             Ridiculus amet habitant gravida lobortis suscipit enim,
-            consectetur quisque. </p>
+            consectetur quisque.
+          </p>
         </div>
         <div>
         </div>
-
-        <TextArea name="questionOne"
-          value={formData.questionOne || ''}
-          onChange={onChange}>Who is the main character"
+        <label>
+          <TextArea name="questionOne"
+            value={formData.questionOne || ''}
+            onChange={onChange}>Who is the main character"
           </TextArea>
-
-
-        <TextArea name="questionTwo"
-          value={formData.questionTwo || ''} onChange={onChange} >"What can you say about the theme of the story?"
+        </label>
+        <label>
+          <TextArea name="questionTwo"
+            value={formData.questionTwo || ''}
+            onChange={onChange} >"What can you say about the theme of the story?"
           </TextArea>
-
-
-        <TextArea name="questionThree"
-          value={formData.questionThree || ''}
-          onChange={onChange}>"Why do you think the author wrote this book?"
+        </label>
+        <label>
+          <TextArea name="questionThree"
+            value={formData.questionThree || ''}
+            onChange={onChange}>"Why do you think the author wrote this book?"
           </TextArea>
-
-
-        <TextArea name="questionFour"
-          value={formData.questionFour || ''}
-          onChange={onChange} >"What do you think is going to happen?"
+        </label>
+        <label>
+          <TextArea name="questionFour"
+            value={formData.questionFour || ''}
+            onChange={onChange} >"What do you think is going to happen?"
           </TextArea>
-
+        </label>
         <span>
           {/* <SubmitBtn  name={"done"} onClick={(e)=>joshClick(e)}>done</SubmitBtn>{/* yehoshua  did it */}
-          {/* {console.log(formData)}  */}
-          <button type="submit">Submit</button>
-          {/* disabled={!isFormValid()} */}
+
+          <button type="submit" >done</button>
+          {/* disabled={!isFormValid()} onSubmit={onSubmit} */}
         </span>
-        {JSON.stringify(formData)}
+        {/* {JSON.stringify(formData)} */}
       </form>
     </div>
   )
--- src/pages/TrainReading/Level/ExerciseQuestion/index.js
+++ src/pages/TrainReading/Level/ExerciseQuestion/index.js
@@ -7,7 +7,14 @@ import Questions from "../../../../components/common/Questions";
 
 export default function QuestionsLevel() {
   const pageInfo = useContext(pageNameContext)
-  pageInfo.setPageName("QuestionsLevel")
+  pageInfo.setPageName("Questions")
+
+  // 1.amount of rounds
+  // 2.true or false on is it the right speed
+  // 3.what speed was chosen 
+  // 4.current speed that he will be tested on 
+  // 5.send answers and speed of reading to db
+
   return (
     <>
       <Questions />
