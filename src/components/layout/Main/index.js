1f97713ace3bce68287d05dc1cff62b31ea877b8
zvilior <102918963+zvilior@users.noreply.github.com>
2022-06-29 16:01:11 +0300

    15:59

--- src/components/layout/Main/index.js
+++ src/components/layout/Main/index.js
@@ -8,7 +8,7 @@ import Page6 from "../../../pages/Assessments/Page6";
 import Page5 from "../../../pages/Assessments/Page5";
 import Page4 from "../../../pages/Assessments/Page4";
 import Page3 from "../../../pages/Assessments/Page3";
-import Header from "../Header";
+import Header from "../header";
 // import RatingAndScore from "../../common/ReatingAndScore";
 // import { Outlet, Route, Routes } from "react-router-dom"
 import TeamH from "../../teams/TeamH";
