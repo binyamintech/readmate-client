56bde67efe6b515f8248b03e52860fd0e862a0a4
נהוראי <nehoraimimon@gmail.com>
2022-06-29 10:51:14 +0300

    submit btn to swith case

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,35 +1,12 @@
-<<<<<<< HEAD
 import Settings from "../../pages/Settings";
+import SubmitBtn from "../common/SubmitBtn";
 
 
-=======
-// import { NavBar } from "../Common/Nav/index";
-
-import Box1 from "../../pages/Dashboard/options/index";
-
-import Dashboard from "../../pages/Dashboard";
-import TeamF from "../teams/TeamF";
-import { dataContext } from "../../context/context";
-import { useContext, useEffect } from "react";
->>>>>>> ba1d921cc6c5923146b50f962af0ef7d3a051968
 function Test() {
-  const localDataContext = useContext(dataContext);
-  useEffect(() => {
-    console.log("das");
-    console.log(localDataContext);
-  });
   return (
     <>
-<<<<<<< HEAD
    <Settings/>
-      
-=======
-      <Box1 />
-      {/* <TeamF /> */}
-      {/* <Options /> */}
-      {/* <Nav /> */}
-      {/* <Dashboard /> */}
->>>>>>> ba1d921cc6c5923146b50f962af0ef7d3a051968
+  
     </>
   );
 }
