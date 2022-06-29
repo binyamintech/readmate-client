aa6a209919d9c1932c375e93437c1268e2a2f622
Your Name <you@example.com>
2022-06-29 15:47:57 +0300

    ddd

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,15 +1,17 @@
 
 import { useContext, useEffect } from 'react';
 import { dataContext } from '../../context/context'
+import YesBtn from '../common/yesBtn';
 function Test() {
-  const localDataContext = useContext(dataContext)
-  useEffect(() => {
-    console.log('data');
-    console.log(localDataContext);
-  })
+  // const localDataContext = useContext(dataContext)
+  // useEffect(() => {
+  //   console.log('data');
+  //   console.log(localDataContext);
+  // })
 
 
   return (<>
+  <YesBtn />
     {/* <Page5 /> */}
   </>)
 }
